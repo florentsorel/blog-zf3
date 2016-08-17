49080a4a734731f2bb05d5325773bfc337714027
Florent Sorel <florent.sorel@gmail.com>
2016-08-17 22:34:14 +0200

    init

---
+++ config/modules.config.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'Zend\ServiceManager\Di',
+    'Zend\Session',
+    'Zend\Mvc\Plugin\Prg',
+    'Zend\Mvc\Plugin\Identity',
+    'Zend\Mvc\Plugin\FlashMessenger',
+    'Zend\Mvc\Plugin\FilePrg',
+    'Zend\Log',
+    'Zend\Form',
+    'Zend\Db',
+    'Zend\Router',
+    'Zend\Validator',
+    'Application',
+];
