729a2e40d5b3bd85c3ae53117f38f0a6b999118b
Florent Sorel <florent.sorel@gmail.com>
2016-09-14 00:02:22 +0200

    feat(backoffice): supprime les configurations inutiles dans le view manager

--- module/Backoffice/config/module.config.php
+++ module/Backoffice/config/module.config.php
@@ -53,11 +53,6 @@ return [
         ],
     ],
     'view_manager' => [
-        'display_not_found_reason' => true,
-        'display_exceptions'       => true,
-        'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
         'template_map' => [
             'layout/backoffice'           => __DIR__ . '/../view/layout/layout.phtml',
         ],
